541f2f55c316db04f3ad1c4d4bb68ff11fbbb7c4
civita <b02502055@ntu.edu.tw>
2017-06-08 14:57:10 +0800

    updateDate.php modified, remove old records before adding

--- server/updateDate.php
+++ server/updateDate.php
@@ -12,6 +12,12 @@ list($month, $day, $year) = split("/", $datepicker, 3);
 $date = date_create();
 date_date_set($date, $year, $month, $day);
 echo date_format($date, 'Y-m-d')."<br>";
+$sql = "DELETE FROM week_date_table WHERE Week IS NOT NULL";
+if ($conn->query($sql) === TRUE) {
+	echo "Old records deleted successfully" . "<br>";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
 for($i=0;$i<18;$i++) {
 	
 	$sql = "INSERT INTO week_date_table (Week, Date) VALUES (\"wk" . $i . "\",\"".$date->format('Y-m-d')."\")"; //TODO update total week number
