32e15e489d178c514aa9caba5523001d2525244d
civita <b02502055@ntu.edu.tw>
2017-06-11 01:22:35 +0800

    querySQL.php modified, remove useless lines

--- server/querySQL.php
+++ server/querySQL.php
@@ -7,20 +7,19 @@ table.db-table td	{ padding:5px; border-left:1px solid #ccc; border-top:1px soli
 $student_id = $_POST[student_id];
 $connection = mysql_connect('localhost','root','plinderhaobunbuncnlab');
 mysql_select_db('classroom',$connection);
-$result = mysql_query('SHOW TABLES',$connection) or die('cannot show tables');
+$result = mysql_query('SHOW TABLES',$connection) or die ('cannot show tables');
 while($tableName = mysql_fetch_row($result)) {
 	$table = $tableName[0];
 	echo '<h3>',$table,'</h3>';
 	if($student_id)
-		$result2 = mysql_query('SELECT * FROM '.$table.' WHERE student_id=\''.$student_id.'\'') or die('cannot show columns from '.$table);
+		$result2 = mysql_query('SELECT * FROM ' . $table . ' WHERE student_id=\'' . $student_id . '\'') or die ('cannot show columns from ' . $table);
 	else
-		$result2 = mysql_query('SELECT * FROM '.$table) or die('cannot show columns from '.$table);
+		$result2 = mysql_query('SELECT * FROM ' . $table) or die ('cannot show columns from ' . $table);
 	if(mysql_num_rows($result2)) {
 		echo '<table cellpadding="0" cellspacing="0" class="db-table">';
-		echo '<tr><th>student_id</th><th>wk0</th><th>wk1</th><th>wk2</th></tr>'; //TODO update total week number
 		while($row2 = mysql_fetch_row($result2)) {
 			echo '<tr>';
-			foreach($row2 as $key=>$value) {
+			foreach($row2 as $key => $value) {
 				echo '<td>',$value,'</td>';
 			}
 			echo '</tr>';
@@ -29,7 +28,7 @@ while($tableName = mysql_fetch_row($result)) {
 	}
 	else {
 		if($student_id)
-			echo 'no record for '.$student_id;
+			echo 'no record for ' . $student_id;
 		else
 			echo 'no record for table classroom';
 	}
